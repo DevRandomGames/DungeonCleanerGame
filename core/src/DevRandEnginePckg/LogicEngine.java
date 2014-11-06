fcc5b603dfc0dba236c343c0843a8c3d4cf624c9
ArclorenSarth <ArclorenSarth@ArclorenSarthPC>
2014-11-06 18:30:47 +0100

    Project Build

---
+++ core/src/DevRandEnginePckg/LogicEngine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DevRandEnginePckg;
+
+/**
+ *
+ * @author ArclorenSarth
+ */
+public class LogicEngine {
+    RenderEngine renderEng;
+    SoundEngine soundEng;
+    LogicEngine logicEng;
+    
+}
---
+++ desktop/src/DungeonCleanerGame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package DungeonCleanerGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import DungeonCleanerGame.DungeonCleaner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonCleaner(), config);
+	}
+}
