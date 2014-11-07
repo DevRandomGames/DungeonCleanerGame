9e93b70d6539a9d0ea71b812fc2dcb42c24ff661
ivanmartinezperez <ivan@192.168.1.37>
2014-11-07 20:02:28 +0100

    Added some stuff and Room Generator on implementation

---
+++ core/src/CharacterPkg/Enemy.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CharacterPkg;
+
+import GameStuff.Characters;
+
+/**
+ *
+ * @author ivan
+ */
+public class Enemy extends Characters {
+    
+    public Enemy(){
+        
+    }
+}
---
+++ core/src/GameStuff/Map.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameStuff;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ivan
+ */
+public class Map {
+    
+    ArrayList GameMap = new ArrayList();
+}
