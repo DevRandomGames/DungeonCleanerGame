2f9a4afe914d2714eca1ab9bc8d6a04f65e40d69
ivanmartinezperez <ivan@192.168.5.241>
2014-11-22 19:45:07 +0100

    Walk animation created, but the program crash: NULLPOINTER

--- core/src/DungeonCleanerGame/CharacterPkg/GameCharacter.java
+++ core/src/DungeonCleanerGame/CharacterPkg/GameCharacter.java
@@ -6,8 +6,14 @@
 
 package DungeonCleanerGame.CharacterPkg;
 
+import static DungeonCleanerGame.CharacterPkg.GameCharacter.dir.right;
+import static DungeonCleanerGame.CharacterPkg.GameCharacter.state.standby;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 /**
@@ -16,17 +22,53 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
  */
 public class GameCharacter extends Actor{
     
+    int sheet_rows;
+    int sheet_columns;
+    String PathToSheet;
+    
+    Texture WalkSheet;
+    
+    public enum state{
+        walk,
+        die,
+        strike,
+        standby;
+    }
+    
+    public enum dir{
+        up,
+        down,
+        right,
+        left
+    }
+    
     Texture texture;
+    public dir d=right;
+    public state st=standby;
     
     private int life;
     private int attack;
     private int defense;
     private int stamina;
+    
+    private Animation WalkUp;
+    private Animation WalkDown;
+    private Animation WalkLeft;
+    private Animation WalkRight;
+    private Animation stndbyleft;
+    private Animation stndbyright ;
+    private Animation stndbyup;
+    private Animation stndbydown;
+    
+    float statetime=1;
+    
   
     
     
     @Override
     public void draw(Batch batch, float alpha){
+        
+     texture = UpdatePlayer(Gdx.graphics.getDeltaTime());
     batch.draw(texture,this.getX(),getY(),this.getOriginX(),this.getOriginY(),this.getWidth(),
             this.getHeight(),this.getScaleX(), this.getScaleY(),this.getRotation(),0,0,
             texture.getWidth(),texture.getHeight(),false,false);
@@ -72,5 +114,57 @@ public class GameCharacter extends Actor{
         this.stamina = stamina;
     }
 
+    public void LoadPlayerTexture(){
+        WalkSheet = new Texture(Gdx.files.internal("Warrior.png"));
+       
+        //NOTE: In Final version put sheet_rows and sheet_colums!
+        TextureRegion[][] tmp = TextureRegion.split(WalkSheet,4,4);
+        
+        WalkDown = new Animation(0.5f,tmp[0]);
+        WalkLeft = new Animation(0.5f,tmp[1]);
+        WalkRight = new Animation(0.5f,tmp[2]);
+        WalkUp = new Animation(0.5f,tmp[3]);
+        
+        stndbydown = new Animation(0, tmp[0][0]);
+        stndbyleft = new Animation(0, tmp[1][0]);
+        stndbyright = new Animation(0, tmp[2][0]);
+        stndbyup = new Animation(0, tmp[3][0]);
+
+     
+    }
+    
+    private Texture UpdatePlayer(float DeltaTime){
+        
+        statetime+=DeltaTime;
+        
+        Animation fm = stndbyright ;
+        boolean loop = true;
+        
+        switch(st){
+            case walk:
+                switch(d){
+                    case down: fm=WalkDown;
+                    case left: fm=WalkLeft;
+                    case right: fm=WalkRight;
+                    case up: fm=WalkUp;    
+                }
+            break;
+            case standby:
+                switch(d){
+                    case down: fm=stndbydown;
+                    case left: fm=stndbyleft;
+                    case right: fm=stndbyright;
+                    case up: fm=stndbyup;    
+                }
+            break;    
+                
+        }
+        
+        return fm.getKeyFrame(statetime, loop).getTexture();
+    
+    }
+    
+    
+    
        
 }
--- core/src/DungeonCleanerGame/ControlsPkg/DungeonPlayerController.java
+++ core/src/DungeonCleanerGame/ControlsPkg/DungeonPlayerController.java
@@ -8,6 +8,8 @@ package DungeonCleanerGame.ControlsPkg;
 
 import DevRandEnginePkg.ControlsEnginePkg.KeyMapper;
 import DevRandEnginePkg.ControlsEnginePkg.PlayerController;
+import static DungeonCleanerGame.CharacterPkg.GameCharacter.dir.*;
+import static DungeonCleanerGame.CharacterPkg.GameCharacter.state.*;
 import DungeonCleanerGame.CharacterPkg.Player;
 import com.badlogic.gdx.Gdx;
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveBy;
@@ -30,15 +32,27 @@ public class DungeonPlayerController extends PlayerController{
         Player pl = (Player) super.ctrlIdentity;
         if(Gdx.input.isKeyPressed(keyMap.key("left"))){
             pl.addAction(moveBy(-1f,0f,0.01f));
+            pl.d = left;
+            pl.st = walk;
+            
         }
         if(Gdx.input.isKeyPressed(keyMap.key("right"))){
             pl.addAction(moveBy(1f,0f,0.01f));
+            pl.d = right;
+            pl.st = walk;
         }
         if(Gdx.input.isKeyPressed(keyMap.key("up"))){
             pl.addAction(moveBy(0f,1f,0.01f));
+            pl.d = up;
+            pl.st = walk;
         }
         if(Gdx.input.isKeyPressed(keyMap.key("down"))){
             pl.addAction(moveBy(0f,-1f,0.01f));
+            pl.d = down;
+            pl.st = walk;
+        }
+        else{
+            pl.st = standby;
         }
     }
     
